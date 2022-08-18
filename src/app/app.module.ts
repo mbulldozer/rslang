54de3b2b92c0482720d05dfd977f9dea81af7df5
Aliaksandr Pauliukavets <mbulldozer@mail.ru>
2022-08-18 19:37:00 +0300

    feat: create navigation and home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './routing/routing.module';
+import { MaterialModule } from './material/material.module';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SidenavListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
+    MaterialModule,
+    RoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
