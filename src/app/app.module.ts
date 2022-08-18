ab8237563f17800221c2e9930ac2763a4b048be8
Aliaksandr Pauliukavets <mbulldozer@mail.ru>
2022-08-18 20:39:05 +0300

    refactor: change structure of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import AppComponent from './app.component';
-import HomeComponent from './home/home.component';
 import RoutingModule from './routing/routing.module';
 import MaterialModule from './material/material.module';
-import HeaderComponent from './navigation/header/header.component';
-import SidenavListComponent from './navigation/sidenav-list/sidenav-list.component';
+
+import AppComponent from './app.component';
+import HomeComponent from './pages/home/home.component';
+import HeaderComponent from './components/header/header.component';
+import SidenavListComponent from './components/sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [
--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-import HomeComponent from '../home/home.component';
+import HomeComponent from '../pages/home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
