9970421a7d5d49e1631138f52b17f88cbef4d0c3
Aliaksandr Pauliukavets <mbulldozer@mail.ru>
2022-08-19 16:50:02 +0300

    feat: add features component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import PopUpComponent from './components/pop-up/pop-up.component';
 import AuthComponent from './components/auth/auth.component';
 import AuthService from './services/auth.service';
 import { PreLoaderComponent } from './components/pre-loader/pre-loader.component';
+import FeaturesComponent from './components/features/features.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { PreLoaderComponent } from './components/pre-loader/pre-loader.component
     PopUpComponent,
     AuthComponent,
     PreLoaderComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule,
